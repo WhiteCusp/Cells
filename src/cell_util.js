d0fb8aa3f39826173198b55a2ec88c67a54c602e
WhiteCusp <bigpet1991@gmail.com>
2014-08-19 00:46:44 +0800

    update util.js

--- src/cell_util.js
+++ src/cell_util.js
@@ -34,12 +34,39 @@
 		},
 
 		random : function(min, max) {
-			if (max == null) {
+			if (max === null) {
 				max = min;
 				min = 0;
 			}
 			return min + Math.floor(Math.random() * (max - min + 1));
-		}
+		},
+
+		scrollTop: function() {
+			return document.documentElement.scrollTop || document.body.scrollTop;
+		},
+
+		getOffset: function(element) {
+      var result = {
+				top:element.offsetTop,
+				left:element.offsetLeft
+      };
+      var parent = element.offsetParent;
+
+      while(parent !== null) {
+        result.top += parent.offsetTop;
+        result.left += parent.offsetLeft;
+        parent = parent.offsetParent;
+      }
+
+      return result;
+    },
+
+    fixEvent: function(ev) {
+    	//ie 6 7 8不存在target、pageX、pageY
+			ev.target = ev.srcElement || ev.target; 
+			ev.pageX = ev.x || ev.pageX;
+			ev.pageY = ev.y || ev.pageY;
+    }
 	};
 
 	return new Cell();
