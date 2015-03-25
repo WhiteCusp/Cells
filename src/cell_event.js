e0304cca6e0b4f0369ea6e3d563d2107f1f2b797
hpzeng <hpzeng@Ctrip.com>
2015-03-25 16:08:19 +0800

    event模块添加注释

--- src/cell_event.js
+++ src/cell_event.js
@@ -16,7 +16,7 @@
 	}
 
 	cellfn = Cell.prototype = {
-
+    // 事件绑定
 		on : function(ele, evTypes, cb) {
       var eleLength = ele.length
 
@@ -43,7 +43,7 @@
 			}
 
 		},
-
+    // 事件解除绑定
 		off : function(ele, evTypes, cb) {
       var eleLength = ele.length
 
@@ -74,7 +74,7 @@
 				}
 			}
 		},
-
+    // 事件触发
 		trigger : function () {
 			var ev = arguments[0],
 				param = Array.prototype.slice.call(arguments,1);
@@ -88,7 +88,7 @@
 			}
 
 		},
-    // 绑定dom事件
+    // 绑定DOM事件
     addEventListener: function(ele, evTypes, cb, useCapture) {
       useCapture = useCapture || false
 
@@ -110,7 +110,7 @@
         }
       }
     },
-
+    // 解除绑定DOM事件
     removeEventListener: function(ele, evTypes, cb, useCapture) {
       useCapture = useCapture || false
 
@@ -126,13 +126,13 @@
       }
     }
 	}
-
+  // 判断是否传统DOM事件
   var isDomEvent=function(obj,evtType){
     if(("on" + evtType).toLowerCase() in obj){
       return true
     } else { return false }
   }
-
+  // 判断是否实现W3C标准的事件监听
   var isW3C = function() {
     return !!document.addEventListener
   }
